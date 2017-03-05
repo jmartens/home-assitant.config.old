9bf2533e520834835c25fd3768d1a483203c18c2
Jonathan Martens <jonathan.martens@maastro.nl>
2017-03-05 22:11:58 +0100

    Update dummy secret generation to be more robust

--- bin/dummy_secrets.sh
+++ bin/dummy_secrets.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo "http_password: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "google_secret: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "pushbullet_api: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "lastfm_api: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "lastfm_user: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "owm_api: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "mqtt_pass: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "mqtt_username: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "ssl_key: ssl_key.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "ssl_cert: ssl_cert.pem" >> $TRAVIS_BUILD_DIR/secrets.yaml
-echo "db_url: dummy" >> $TRAVIS_BUILD_DIR/secrets.yaml
+# Generate secret for all requried entries to prevent failure
+secrets=$(grep -hRP \!secret {*.yaml,*/*.yaml} | \
+    sed -re 's/\s*\w+\:\s+\!secret\s+(\w+)/\1/' | \
+    sort -u )
+for secret in $secrets
+do
+    case $secret in
+        longitude | latitude | *long | *lat | home_elevation)
+            echo "$secret: 0" | tee -a $TRAVIS_BUILD_DIR/secrets.yaml;;
+        timezone)
+            echo "$secret: Europe/Amsterdam" | tee -a $TRAVIS_BUILD_DIR/secrets.yaml;;
+        *)
+            echo "$secret: dummy" | tee -a $TRAVIS_BUILD_DIR/secrets.yaml;;
+    esac
+done
+
+# Create certificate requriements
 touch "ssl_key.pem"
 touch "ssl_cert.pem"
