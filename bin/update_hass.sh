ffec2b84c394d2c43d3f1eea4ebe023564de91bb
Jonathan Martens <jonathan@snetram.nl>
2017-03-20 11:14:55 +0100

    Retry on update_hass

--- bin/update_hass.sh
+++ bin/update_hass.sh
@@ -3,10 +3,10 @@
 sudo systemctl stop home-assistant.service
 
 # Update Home Assistant 
-sudo -su hass
-source /srv/hass/hass_venv/bin/activate
-pip3 install --upgrade homeassistant
-deactivate
+sudo -u hass `source /srv/hass/hass_venv/bin/activate 2>&1 && \
+    pip3 install --upgrade homeassistant 2>&1 && \  source
+    /srv/hass/hass_venv/bin/deactivate 2>&1 | \
+    tee /home/hass/.homeassistant/hass-update.log`
 
 # Start service
 sudo systemctl start home-assistant.service
